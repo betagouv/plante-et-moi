11e463c920515c594d0ade307b81b89c3af60056
Julien DAUPHANT <judauphant@gmail.com>
2016-12-15 16:38:09 +0100

    Multi site

---
+++ vegetalisez/js/main.js
@@ -0,0 +1,37 @@
+
+var city = getParameterByName("c");
+if(city == null) {
+	city = "Arles";
+}
+var cityWithParticle;
+if(isVowelOrH(city[0])) {
+	cityWithParticle = "d'"+city;
+} else {
+	cityWithParticle = 'de '+city;
+}
+document.getElementById('header-title').innerHTML += ' Végétalisez '+city;
+document.title += ' - Ville '+cityWithParticle;
+	
+var center;
+var codePostaux = [];
+var codeInsee;
+var contour;
+
+var requestCityGeoData = new XMLHttpRequest();
+requestCityGeoData.open('GET', 'https://geo.api.gouv.fr/communes?fields=code,nom,codesPostaux,centre,contour&nom='+city, true);
+
+requestCityGeoData.onload = function() {
+    if (requestCityGeoData.status >= 200 && requestCityGeoData.status < 400) {
+    // Success!
+        var data = JSON.parse(requestCityGeoData.responseText);
+        codePostaux = data[0]["codesPostaux"];
+        codeInsee = data[0]["code"];
+        center = data[0]["centre"]["coordinates"];
+        contour = data[0]["contour"];
+        try { onCityGeoDataLoaded(); } catch(e){}
+    } else {
+    // We reached our target server, but it returned an error
+
+    }
+};
+requestCityGeoData.send();
