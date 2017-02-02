5f4b79aea7fc1b8926b45b0e424331aeac7a8897
Julien DAUPHANT <judauphant@gmail.com>
2017-02-02 10:27:31 +0100

    Fix postgresql character type

--- backend/conf/evolutions/default/1.sql
+++ backend/conf/evolutions/default/1.sql
@@ -1,14 +1,19 @@
 # --- !Ups
-
 CREATE TABLE review (
-    application_id character(100) NOT NULL,
-    agent_id character(100) NOT NULL,
+    application_id character varying(100) NOT NULL,
+    agent_id character varying(100) NOT NULL,
     creation_date date NOT NULL,
     favorable boolean NOT NULL,
     comment text NOT NULL,
     PRIMARY KEY (application_id, agent_id)
 );
 
-# --- !Downs
+CREATE TABLE application_extra (
+    application_id character varying(100) NOT NULL,
+    status character varying(100) NOT NULL,
+    PRIMARY KEY (application_id)
+);
 
+# --- !Downs
+DROP TABLE application_extra;
 DROP TABLE review;
