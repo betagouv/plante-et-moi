6a97a5e0f246640a9a5c5c723d894dec67dc9786
Julien DAUPHANT <judauphant@gmail.com>
2017-03-25 23:50:13 +0100

    Add city to backend

--- backend/app/models/Agent.scala
+++ backend/app/models/Agent.scala
@@ -8,6 +8,7 @@ import play.api.libs.json._
 import play.api.libs.json.Reads._
 
 case class Agent(id: String,
+                 city: String,
                  name: String,
                  qualite: String,
                  email: String,
@@ -22,16 +23,12 @@ class AgentService @Inject()(configuration: play.api.Configuration, settingServi
   private lazy val cryptoSecret = configuration.underlying.getString("play.crypto.secret")
 
   private implicit def resultReads(city: String): Reads[Agent] = {
-    JsPath.json.update((JsPath \ 'key).json.copyFrom((JsPath \ 'email).json.pick[JsString].map{ jsString => JsString(Hash.sha256(s"${jsString.value}$city$cryptoSecret")) })) andThen Json.reads[Agent]
+    JsPath.json.update(
+      (JsPath \ 'key).json.copyFrom((JsPath \ 'email).json.pick[JsString].map{ jsString => JsString(Hash.sha256(s"${jsString.value}$city$cryptoSecret")) })
+    ) andThen JsPath.json.update(
+      (JsPath \ 'city).json.put(JsString(city))
+    ) andThen Json.reads[Agent]
   }
-
-  private lazy val agents = List(
-    Agent("admin", "Jean Paul", "service développement durable", "jean.paul.durable@example.com", Hash.sha256(s"${cryptoSecret}Iz7h_09MqC"), true, true, false, false),
-    Agent("verts", "Jeanne D'arc", "direction des espaces verts-propreté", "jeanne.d-arc.verts@example.com", Hash.sha256(s"${cryptoSecret}nYX3aeNs"), false, false, true, false),
-    Agent("voirie", "Jeanne D'arc", "service de la voirie", "jeanne.d-arc.voirie@example.com", Hash.sha256(s"${cryptoSecret}o-35qG5T"), false, false, true, false),
-    Agent("public", "Jeanne D'arc", "direction occupation du domaine public", "jeanne.d-arc.public@example.com", Hash.sha256(s"{cryptoSecret}_eMMIKc9"), false, false, true, false),
-    Agent("elu", "Richard Dupont", "adjoint au maire parcs et jardins", "jdupont.elu@example.com", Hash.sha256(s"${cryptoSecret}Bv5_75R"), true, false, true, true)
-  )
 
   def all(city: String) = {
     implicit val agentReads = resultReads(city)
